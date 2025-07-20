1ae82f65efad6bcf794cc6fe7ff4b005250aba39
Sandeep Sharma <sandeeptech8@gmail.com>
2025-07-20 16:18:31 +0530

    feat: setup eleventy

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  // ⏳ Add a 'readableDate' filter using Luxon
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("LLLL d, yyyy"); // "July 20, 2025"
+  });
+
+  eleventyConfig.addPassthroughCopy("site.css");
+  eleventyConfig.addPassthroughCopy("greeting.mp3");
+  eleventyConfig.addPassthroughCopy("resume");
+  eleventyConfig.addPassthroughCopy(
+    "SandeepSharma_SoftwareDeveloper_Resume.pdf"
+  );
+
+  eleventyConfig.addCollection("logs", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("./logs/*.md");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "docs",
+    },
+  };
+};
